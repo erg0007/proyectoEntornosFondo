7c177cdde31c4e3518ea03b598373cd542055f3d
kiko <fmo@gmail.com>
2022-04-25 10:50:28 +0200

    Metodos: adelantar y atrasar la lista clientes

--- proyectoEntornosFondo/src/proyectoentornosfondo/ListaClientes.java
+++ proyectoEntornosFondo/src/proyectoentornosfondo/ListaClientes.java
@@ -11,5 +11,24 @@ public class ListaClientes {
         this.tamanioLista = tamanioLista;
         this.listaClientes = new Cliente[tamanioLista];
     }
+    public void adelantarPuesto(Cliente adelantado) {
+    for (int i = 0; i < listaClientes.length; i++) {
+        if (adelantado.getNumLista() > listaClientes[i].getNumLista()) {
+            listaClientes[i].setNumLista(-1);
+            i = listaClientes.length;
+            adelantado.setNumLista(+1);
+        }
+    }
+}
+
+public void atrasarPuesto(Cliente atrasado) {
+    for (int i = 0; i < listaClientes.length; i++) {
+        if (atrasado.getNumLista() > listaClientes[i].getNumLista()) {
+            listaClientes[i].setNumLista(+1);
+            i = listaClientes.length;
+            atrasado.setNumLista(-1);
+        }
+    }
+}
 }
 //Termina parte de Angel
